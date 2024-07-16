a764834412214d04435f03cdec9deb417e97fb57
shubh-xmate <shuklashubham2277@gmail.com>
2024-07-16 16:09:05 +0530

    added all the other functionalities options

--- payrollfrontend/src/app/app.routes.ts
+++ payrollfrontend/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { ProfileComponent } from './components/dashboard/main-content/profile/pr
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LeaveRequestComponent } from './components/dashboard/main-content/leave-request/leave-request.component';
+import { LeaveDetailsComponent } from './components/dashboard/main-content/leave-details/leave-details.component';
+import { PayrollDetailsComponent } from './components/dashboard/main-content/payroll-details/payroll-details.component';
+import { GeneratePayrollComponent } from './components/dashboard/main-content/generate-payroll/generate-payroll.component';
+import { CreateEmployeeComponent } from './components/dashboard/main-content/create-employee/create-employee.component';
+import { RequestedLeavesComponent } from './components/dashboard/main-content/requested-leaves/requested-leaves.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -13,7 +18,12 @@ export const routes: Routes = [
         children: [
           { path: '', redirectTo: 'profile', pathMatch: 'full' },
           { path: 'profile', component: ProfileComponent },
-          { path: 'leave-request', component:  LeaveRequestComponent}
+          { path: 'leave-request', component:  LeaveRequestComponent},
+          { path: 'leave-details', component:  LeaveDetailsComponent },
+          { path: 'payroll-details', component:  PayrollDetailsComponent},
+          { path: 'generate-payroll', component: GeneratePayrollComponent },
+          { path: 'create-employee', component:  CreateEmployeeComponent},
+          { path: 'requested-leaves', component: RequestedLeavesComponent }
         ]
       }
 ];
---
+++ payrollfrontend/src/app/components/dashboard/main-content/generate-payroll/generate-payroll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-payroll',
+  standalone: true,
+  imports: [],
+  templateUrl: './generate-payroll.component.html',
+  styleUrl: './generate-payroll.component.css'
+})
+export class GeneratePayrollComponent {
+
+}
