dc158d73733edee5bdd9395e0d097d208a27fcee
Parneet Singh <parneet.a.singh@ukg.com>
2024-07-16 12:23:56 +0530

    modified sidebar

--- payrollfrontend/src/app/components/dashboard/dashboard.component.ts
+++ payrollfrontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, MainContentComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
