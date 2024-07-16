f5b34ec6c70096bec9fc7b42f31b95a3ec234af2
shubh-xmate <shuklashubham2277@gmail.com>
2024-07-16 15:35:43 +0530

    added sidebar component

--- payrollfrontend/src/app/app.component.ts
+++ payrollfrontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, LoginComponent],
+  imports: [RouterModule, RouterOutlet, DashboardComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
