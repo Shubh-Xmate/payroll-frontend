9320f777329179fb019b3377a9e1f03eaabcf46a
shubh-xmate <shuklashubham2277@gmail.com>
2024-07-17 12:44:04 +0530

    added success and failure alerts

--- payrollfrontend/src/app/components/dashboard/main-content/leave-request/leave-request.component.ts
+++ payrollfrontend/src/app/components/dashboard/main-content/leave-request/leave-request.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LeaveDto } from './leave-request.model';
 
 @Component({
   selector: 'app-leave-request',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './leave-request.component.html',
   styleUrl: './leave-request.component.css'
 })
 export class LeaveRequestComponent {
 
-}
+  leaveDetails: LeaveDto = {
+    employeeId: 0,
+    leaveType: '',
+    startDate: new Date(),
+    endDate: new Date(),
+    appliedDate: new Date(),
+    status: '',
+    comments: '',
+    managerId: 0
+  };
+
+  sentSuccessfully: boolean = false;
+  showDetails: boolean = false;
+
+  constructor() {}
+
+  onSubmit() {
+    this.leaveDetails.employeeId = 1;
+    this.leaveDetails.status = "pending";
+    this.leaveDetails.managerId = 2;
+    this.leaveDetails.appliedDate = new Date();
+    console.log(this.leaveDetails);
+    this.showDetails = true;
+    if(this.leaveDetails.leaveType == 'abc') this.sentSuccessfully = true;
+    else this.sentSuccessfully = false;
+    // You can add your service call here to submit the form data
+  }
+};
