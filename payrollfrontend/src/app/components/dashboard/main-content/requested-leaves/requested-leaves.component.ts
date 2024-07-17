6efe44547dcc03c6948e521c4a982132d6816a7c
shubh-xmate <shuklashubham2277@gmail.com>
2024-07-17 14:54:36 +0530

    updated dimension and added leave-status cards on status

--- payrollfrontend/src/app/components/dashboard/main-content/requested-leaves/requested-leaves.component.ts
+++ payrollfrontend/src/app/components/dashboard/main-content/requested-leaves/requested-leaves.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-requested-leaves',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './requested-leaves.component.html',
   styleUrl: './requested-leaves.component.css'
 })
-export class RequestedLeavesComponent {
 
+export class RequestedLeavesComponent {
+  tabs: string[] = ['All', 'Pending', 'Accepted', 'Declined'];
+  activeTab: number = 0;
+  tabData: any[] = [];
+  dataLoaded: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fetchDataForTab(this.activeTab);
+  }
+
+  changeTab(index: number): void {
+    this.activeTab = index;
+    this.fetchDataForTab(index);
+  }
+
+  fetchDataForTab(index: number): void {
+    this.dataLoaded = true;
+    this.tabData = [
+      {
+        title: "first",
+        description: "description1"
+      },
+      {
+        title: "first2",
+        description: "description2"
+      },
+      {
+        title: "first3",
+        description: "description3"
+      }
+    ]
+    console.log(this.tabData);
+    console.log(index);
+  }
 }
